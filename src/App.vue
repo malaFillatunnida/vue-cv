5043f8206aee5ddbaff7729688e109efcfd8706e
malaFillatunnida <fillatunnida@gmail.com>
2022-03-14 11:21:13 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row py-4">
+      <Profil />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Profil from "./components/Profil.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Profil,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Profil.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="all" style="padding: 30px">
+    <div class="head">
+      <h1
+        style="
+          font-family: times new roman;
+          text-align: left;
+          color: white;
+          margin-left: 390px;
+        "
+      >
+        MALA <br /><span> FILLATUNNIDA</span>
+      </h1>
+    </div>
+
+    <div class="border3">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsr2qne80colViST5F7DDb14m8CM3AhI690w&usqp=CAU"
+        style="
+          border: 8px solid white;
+          border-radius: 100%;
+          width: 200px;
+          height: 200px;
+          margin-top: -255px;
+        "
+      />
+
+      <h2>PROFILE</h2>
+      <p><b>NAME</b></p>
+      <p>Mala Fillatunnida</p>
+      <p><b>DATE OF BIRTH</b></p>
+      <p>17 October 2005</p>
+      <p><b>ADDRESS</b></p>
+      <p>
+        Pancur Village, Pancur Sub-District, Rembang District, Central Java
+        Province
+      </p>
+      <p><b>WEBSITE</b></p>
+      <p>www.malafll.com</p>
+
+      <h2>AWARDS</h2>
+      <p><b>CHARTER AWARD 2012 </b></p>
+      <p>malafilla.com</p>
+      <p><b>INTERFACE DESIGN 2013 </b></p>
+      <p>flltnnda.com</p>
+
+      <h2>CONTACT</h2>
+      <p><b> HOME NUMBER &#9742;</b></p>
+      <p>(0411) 123 667</p>
+      <p><b>PHONE NUMBER &#x1F4DE;</b></p>
+      <p>+62 88227561539</p>
+      <p><b>EMAIL &#x1F4E7;</b></p>
+      <p>malafillatunnidagmail.com</p>
+
+      <h2>SOCIAL</h2>
+      <p><b>TWITTER</b></p>
+      <p>@malafilla</p>
+      <p><b>FACEBOOOK </b></p>
+      <p>mala_filla</p>
+    </div>
+
+    <div class="border4">
+      <div id="p" style="height: 180px">
+        <h2>ACHIEVEMENT</h2>
+        <div class="ac">
+          <h3>COMPETITION 2016</h3>
+          <p>
+            1st winner of the Quran reading competition at the Pancur District
+            level (2016).
+          </p>
+        </div>
+        <div class="ac">
+          <h3>COMPETITION 2016</h3>
+          <p>
+            3rd place in the Rembang District level Quran reading competition
+            (2016).
+          </p>
+        </div>
+        <div class="ac">
+          <h3>COMPETITION 2018</h3>
+          <p>
+            1st winner of the Quran reading competition at the Pancur District
+            level (2018).
+          </p>
+        </div>
+      </div>
+
+      <br />
+      <div id="p" style="height: 170px">
+        <h2>EDUCATIONAL BACKGROUND</h2>
+        <p><b>2010 - 2012</b> : TK Kartika Sari</p>
+        <p><b>2012 - 2018</b> : MI Al - Hamidiyyah</p>
+        <p><b>2018 - 2021</b> : SMP N 1 Pancur</p>
+        <p><b>Now</b> : SMK Bina Nusantara Demak</p>
+      </div>
+      <br />
+      <div id="p">
+        <h2>WORDS OF WISDOM</h2>
+        <div class="wr">
+          <h3>SUNDAY, 1 OCTOBER 2021</h3>
+          <p>
+            “ If in oneself there is still a sense of shame and fear to do
+            something good, it is a guarantee for that person is not going to
+            the meeting he had with the progress of a single step. Thousands of
+            candles can be lit by a single candle, and the candle will not be
+            short-lived. Happiness will not be reduced even if every moment
+            divided.”
+          </p>
+        </div>
+        <div class="wr">
+          <h3>MONDAY, 2 OCTOBER 2021</h3>
+          <p>
+            “ People who managed to take advantage of the mistakes that he did,
+            and will try again to perform in a different way. Be the man who at
+            birth everyone laughing happily, but only you yourself were crying
+            and the death of all the sad cry, but only you yourself smiling.“
+          </p>
+        </div>
+        <div class="wr">
+          <h3>TUESDAY, 3 OCTOBER 2021</h3>
+          <p>
+            “ The real threat is actually not when the computer begins to think
+            like a human, but when humans begin to think like computers. The
+            real threat is actually not when the computer begins to think like a
+            human, but when humans begin to think like computers. “
+          </p>
+        </div>
+      </div>
+
+      <br />
+      <div id="p">
+        <h2>SKILLS</h2>
+        <div class="skil">
+          <h3>PROFESSIONAL SKILLS</h3>
+          <p>Writing Book &nbsp; &#9899; &#9899; &#9899;</p>
+          <p>Reading Book &nbsp; &#9899; &#9899; &#9899;</p>
+          <p>Communication &nbsp; &#9899; &#9899; &#9899;</p>
+        </div>
+
+        <div class="skil">
+          <h3>PERSONAL SKILLS</h3>
+          <p>Organization &nbsp; &#9899; &#9899; &#9899;</p>
+          <p>Commitment &nbsp; &#9899; &#9899; &#9899;</p>
+          <p>Multitasking &nbsp; &#9899; &#9899; &#9899;</p>
+        </div>
+      </div>
+      <br />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* .resume {
+  border-top: 10px solid white;
+  color: white;
+  border-left: none;
+  border-bottom: none;
+  border-right: none;
+  margin-bottom: 10px;
+} */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.head {
+  background-color: darkslategray;
+  padding: 20px;
+}
+.border3 {
+  border: 1px solid black none;
+  padding: 20px;
+  width: 350px;
+  background-color: white;
+  float: left;
+  text-align: right;
+}
+.border4 {
+  border: 1px solid black none;
+  padding: 20px;
+  float: left;
+  width: 66%;
+  height: 894px;
+  margin-bottom: 50px;
+}
+#p {
+  padding: 20px;
+  height: 320px;
+}
+h2 {
+  font-family: "Times New Roman", Times, serif;
+  background-color: darkslategray;
+  padding: 12px;
+  border-radius: 7px;
+  color: white;
+}
+.wr {
+  float: left;
+  width: 245px;
+  padding: 5px;
+}
+.ac {
+  float: left;
+  width: 30%;
+  padding: 5px;
+}
+.skil {
+  float: left;
+  width: 40%;
+  height: 300px;
+}
+</style>
